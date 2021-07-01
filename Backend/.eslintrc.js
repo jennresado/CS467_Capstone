073208e0cbd6766bafa6d001d47df4fa6c76dde1
Jessica Duell <jessica.duell@pendo.io>
2021-07-01 16:54:50 -0400

    fixed eslint issues

---
+++ Backend/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'space'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
