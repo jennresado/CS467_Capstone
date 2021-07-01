450a0a243a7f405ad5075216a3b4d8210c6c4f67
Jessica Duell <jessica.duell@pendo.io>
2021-07-01 16:59:58 -0400

    issue fixed

--- Backend/db/dbconfig.js
+++ Backend/db/dbconfig.js
@@ -3,4 +3,4 @@ const knex = require('knex');
 const config = require('../knexfile');
 const environment = process.env.DB_ENV || 'development';
 
-module.exports = knex(config[environment])
+module.exports = knex(config[environment]);
