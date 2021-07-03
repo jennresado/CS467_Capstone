87cd1f8da6dcdffa78fa96f5eaa04572c8806838
jennresado <doje@oregonstate.edu>
2021-07-03 00:04:49 -0700

    setup react framework

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-const express     = require("express"),
-      app         = express(),
-      handlebars  = require("express-handlebars").create({defaultLayout: "main"})
-      bodyParser  = require("body-parser"),
-      path        = require('path'),
-      PORT        = process.env.PORT || 5000;
+const express = require('express')
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-app.engine("handlebars", handlebars.engine);
-app.set("view engine", "handlebars");
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-  
-// ROOT ROUTE
-app.get("/", function(req, res, next){
-    res.render('home');
-});
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(function(req, res){
-    res.status(404);
-    res.send('404');
-});
-
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500);
-    res.send('500');
-});
-
-app.listen(PORT, function(){
-    console.log(`Express started on ${PORT}; press Ctrl-C to terminate.`);
-});
+app.listen(port, () => console.log(`App is live on port ${port}!`))
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className='container'>
+        <h1>Testing</h1>
+        {/* Navigation Menu */}
+        {/* Landing Page */}
+        {/* Sign Up Page */}
+        {/* Login Page */}
+        {/* Dashboard Page */}
+        {/* Profile Settings Page */}
+        {/* Add Animal Page */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
