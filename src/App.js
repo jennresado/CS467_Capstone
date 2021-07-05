fd47059e9fe8f6316091a6681a0027d2e8da0488
kylee-hal <halekyl@oregonstate.edu>
2021-07-05 18:01:23 -0400

    Added Navigation and Routes for testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +15,11 @@ function App() {
         {/* Dashboard Page */}
         {/* Profile Settings Page */}
         {/* Add Animal Page */}
+        <Route path= '/Landing' component={Footer}/>
+        <Route path= '/Landing' component={Navigation}/>
+        <Route path= '/UserProfile' component={Navigation}/>
+        <Route path= '/UserProfile' component={Footer}/>
+
       </div>
     </Router>
   );
