8ad345a527af9aae3deef9e7a9d66d17fa44a17a
kylee-hal <halekyl@oregonstate.edu>
2021-07-05 17:59:29 -0400

    Made so that certain links don't show on Landing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Navbar, Nav } from 'react-bootstrap'
-import { Link } from "react-router-dom"
+import { useLocation } from 'react-router-dom' 
 
+// can change to <Nav.Item </Nav.Item> so the links are not 'blue'
+//Footer is same for Landing, Login & SignUp
+//User Profile Settings adds 'Dashboard', 'Profile' & 'Animal' 
 const Footer = () => {
+    const location = useLocation()
     return (
         <div>
             <Navbar collapseOnSelect expand= 'sm'             
@@ -11,11 +15,14 @@ const Footer = () => {
                 fixed='bottom'>
                 <Navbar.Toggle />
                 <Navbar.Collapse className='justify-content-center'>
-                <Nav.Link href="#Home">Home</Nav.Link>
+                {location.pathname === '/UserProfile' && <Nav.Link href="#Dashboard">Dashboard</Nav.Link> }
+                <Nav.Link href="#Home">Home</Nav.Link> 
                 <Nav.Link href="#About">About</Nav.Link>
                 <Nav.Link href="#Contact">Contact</Nav.Link>
-                <Nav.Link href="#Login">Login</Nav.Link>
-                <Nav.Link href="#SignUp">SignUp</Nav.Link>
+                {location.pathname === '/Landing' && <Nav.Link href="#Login">Login</Nav.Link> }
+                {location.pathname === '/UserProfile' && <Nav.Link href="#Profile">Profile</Nav.Link> }
+                {location.pathname === '/UserProfile' && <Nav.Link href="#Animal">Animal</Nav.Link> }
+                {location.pathname === '/Landing' && <Nav.Link href="#SignUp">SignUp</Nav.Link> }
                 </Navbar.Collapse>
             </Navbar>
         </div>
