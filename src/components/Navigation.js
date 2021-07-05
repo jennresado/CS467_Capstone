968f5a5a6e6674c0b049764289a0df147e2cdaf5
kylee-hal <halekyl@oregonstate.edu>
2021-07-05 17:59:39 -0400

    Made so that certain links don't show on Landing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Navbar, Nav } from 'react-bootstrap'
+import { useLocation } from 'react-router-dom' 
 
+ // can change to <Nav.Item </Nav.Item> so the links are not 'blue'
 
- 
+ //Landing page does not show Dashboard Nav Link 
+
  const Navigation = () => {
-     return (
+    const location = useLocation()
+    return (
          <div className='navBrand'> 
             <Navbar collapseOnSelect expand= 'sm'
                 bg='white' 
@@ -12,8 +16,12 @@ import { Navbar, Nav } from 'react-bootstrap'
                 fixed='top'>
                 <Navbar.Toggle />
                 <Navbar.Collapse className='justify-content-end'>
-                <Nav.Link href="#Login">Login</Nav.Link>
-                <Nav.Link href="#SignUp">SignUp</Nav.Link>
+                {location.pathname === '/UserProfile' && <Nav.Link href="#Dashboard">Dashboard</Nav.Link> }
+                {location.pathname === '/UserProfile' && <Nav.Link href="#Animal">Animal</Nav.Link> }
+                {location.pathname === '/UserProfile' && <Nav.Link href="#Profile">Profile</Nav.Link> }
+                {location.pathname === '/Landing' && <Nav.Link href="#Login">Login</Nav.Link> }
+                {location.pathname === '/UserProfile' && <Nav.Link href="#Logout">Logout</Nav.Link> }
+                {location.pathname === '/Landing' && <Nav.Link href="#SignUp">SignUp</Nav.Link> }
                 </Navbar.Collapse>
             </Navbar>
             <div className='navContent'> </div>
