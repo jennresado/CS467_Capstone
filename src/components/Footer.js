ad5aac81f45bb7f80a1610ad59484a2a29713470
kylee-hal <halekyl@oregonstate.edu>
2021-07-05 16:44:06 -0400

    initial Footer addition

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar, Nav } from 'react-bootstrap'
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+    return (
+        <div>
+            <Navbar                 
+                bg='white' 
+                variant='light'
+                fixed='bottom'>
+                <Navbar.Toggle />
+                <Navbar.Collapse className='justify-content-center'>
+                <Nav.Link href="#Home">Home</Nav.Link>
+                <Nav.Link href="#About">About</Nav.Link>
+                <Nav.Link href="#Contact">Contact</Nav.Link>
+                <Nav.Link href="#Login">Login</Nav.Link>
+                <Nav.Link href="#SignUp">SignUp</Nav.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Footer
