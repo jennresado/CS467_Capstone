61b343b49137215d170d006a5d8c4f677b875fda
kylee-hal <halekyl@oregonstate.edu>
2021-07-05 16:43:53 -0400

    initial addition Navigiation

---
+++ src/components/Navigation.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar, Nav } from 'react-bootstrap'
+
+
+ 
+ const Navigation = () => {
+     return (
+         <div className='navBrand'> 
+            <Navbar 
+                bg='white' 
+                variant='light'
+                fixed='top'>
+                <Navbar.Toggle />
+                <Navbar.Collapse className='justify-content-end'>
+                <Nav.Link href="#Login">Login</Nav.Link>
+                <Nav.Link href="#SignUp">SignUp</Nav.Link>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className='navContent'> </div>
+        </div>
+     )
+ }
+ 
+ export default Navigation
+ 
