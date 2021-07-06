390781682890f2474436cec88fba9dba6f3d649e
kylee-hal <halekyl@oregonstate.edu>
2021-07-06 11:59:39 -0400

    Added Contact Us page

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+const Contact = () => {
+    return (
+        <div className='contactPage'>
+            <div className='row justify-content-center'>
+                <div className='col-xl-8'>
+                    <div className='row'>
+                        <h1>Contact Us</h1>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Contact
+
