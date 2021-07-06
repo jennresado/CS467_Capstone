00419b32c6997a07b05c0d6034a3b60d912b7f5e
Jessica <duellje@oregonstate.edu>
2021-07-05 23:18:22 -0400

    fixed

--- Backend/users/usersRouter.js
+++ Backend/users/usersRouter.js
@@ -1,325 +1,75 @@
-const supertest = require("supertest");
-const server = require("./server");
-const db = require("../db/dbconfig");
+const router = require("express").Router();
+const Users = require("./usersModel");
+const helpers = require("./userHelpers");
 
-describe("server", () => {
-  //wipes all tables in database clean so each test starts with empty tables
-  beforeEach(async () => {
-    //db is the knex initialized object using db.raw to truncate postgres tables with foreign keys
-    //can use knex.raw but it is global and deprecated
-    await db.raw("TRUNCATE TABLE animals RESTART IDENTITY CASCADE");
-    await db.raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE");
-    await db.raw("TRUNCATE TABLE user_animals RESTART IDENTITY CASCADE");
-  });
-
-  describe("GET /", () => {
-    it("returns 200 OK", async () => {
-      const res = await supertest(server).get("/");
-      expect(res.status).toBe(200);
-    });
-
-    it("returns json object {server: working}", async () => {
-      const res = await supertest(server).get("/");
-      expect(res.body.server).toBe("working");
-    });
-  });
-
-  describe("Authorization", () => {
-    describe("POST /register", () => {
-      it("adds a new user to an empty db", async () => {
-        const users = await db("users");
-        expect(users).toHaveLength(0);
-
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-
-        const newUsers = await db("users");
-        expect(newUsers).toHaveLength(1);
-      });
-
-      it("adds a new user to a db with users in it", async () => {
-        const users = await db("users");
-        expect(users).toHaveLength(0);
-
-        await db("users").insert({
-          username: "pippin",
-          password: "pass",
-          first_name: "Pippin",
-          last_name: "Took",
-          email: "Took@gmail.com",
-          admin: false,
-        });
-        await db("users").insert({
-          username: "frodo",
-          password: "pass",
-          first_name: "Frodo",
-          last_name: "Baggins",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-        await db("users").insert({
-          username: "merry",
-          password: "pass",
-          first_name: "Merry",
-          last_name: "Took",
-          email: "tookM@gmail.com",
-          admin: false,
-        });
-
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "gamgee@gmail.com",
-          admin: false,
-        });
-
-        const newUsers = await db("users");
-        expect(newUsers).toHaveLength(4);
-      });
-
-      it("returns 201 OK when user is created sucessfully", async () => {
-        const res = await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "gamgee@gmail.com",
-          admin: false,
-        });
-
-        expect(res.status).toBe(201);
-      });
-
-      it("returns token", async () => {
-        const res = await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "gamgee@gmail.com",
-          admin: false,
-        });
-
-        expect(res.body.token).not.toBeNull();
-      });
-
-      it("returns 400 when user has no username", async () => {
-        const res = await supertest(server).post("/auth/register").send({
-          password: "pass",
-        });
-
-        expect(res.status).toBe(400);
-      });
-
-      it("returns an error message in res.body when user has no username", async () => {
-        const res = await supertest(server).post("/auth/register").send({
-          password: "pass",
-        });
-
-        expect(res.body.error).toBe(
-          "The request object attributes have one or more of the wrong type",
-        );
-      });
-
-      it("returns 400 when user has no password", async () => {
-        const res = await supertest(server).post("/auth/register").send({
-          username: "sam",
-        });
-
-        expect(res.status).toBe(400);
-      });
-
-      it("returns an error message in res.body when user has no password", async () => {
-        const res = await supertest(server).post("/auth/register").send({
-          username: "pass",
-        });
-
-        expect(res.body.error).toBe(
-          "The request object attributes have one or more of the wrong type",
-        );
-      });
-
-      it("returns 400 when user has no username and no password ", async () => {
-        const res = await supertest(server).post("/auth/register").send({});
-
-        expect(res.status).toBe(400);
-      });
-
-      it("returns an error message in res.body when user has no username and no password", async () => {
-        const res = await supertest(server).post("/auth/register").send({});
-
-        expect(res.body.error).toBe(
-          "The request object attributes have one or more of the wrong type",
-        );
+router.get("/", (req, res) => {
+  const username = req.jwt.username;
+  Users.getUserBy("username", username)
+    .then((user) => {
+      res.status(200).json({ user });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err.message,
+        errorMessage: "Could not retrive that user",
+        stack: "Users router line 16",
       });
     });
+});
 
-    describe("POST /login", () => {
-      it("returns 200 OK when logging in successfully", async () => {
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
+router.put("/", helpers.validateUserEdit, (req, res) => {
+  const username = req.jwt.username;
+  const changes = req.body;
 
-        const res = await supertest(server).post("/auth/login").send({
-          username: "sam",
-          password: "pass",
-        });
-
-        expect(res.status).toBe(200);
-      });
-
-      it("returns Welcome message in res.body when logging in successfully", async () => {
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-
-        const res = await supertest(server).post("/auth/login").send({
-          username: "sam",
-          password: "pass",
-        });
-
-        expect(res.body.message).toBe("Welcome");
-      });
-
-      it("returns token in res.body when logging in successfully", async () => {
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-
-        const res = await supertest(server).post("/auth/login").send({
-          username: "sam",
-          password: "pass",
-        });
-
-        expect(res.body.token).not.toBeNull();
-      });
-
-      it("returns 400 when no username is provided", async () => {
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-
-        const res = await supertest(server).post("/auth/login").send({
-          password: "pass",
-        });
-
-        expect(res.status).toBe(400);
-      });
-
-      it("returns error message in res.body when no username is provided", async () => {
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-
-        const res = await supertest(server).post("/auth/login").send({
-          password: "pass",
-        });
-
-        expect(res.body.error).toBe(
-          "The request object is missing one or more required attributes",
-        );
-      });
-
-      it("returns 400 when no password is provided", async () => {
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-
-        const res = await supertest(server).post("/auth/login").send({
-          username: "pass",
-        });
-
-        expect(res.status).toBe(400);
-      });
-
-      it("returns error message in res.body when no password is provided", async () => {
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-
-        const res = await supertest(server).post("/auth/login").send({
-          username: "pass",
-        });
-
-        expect(res.body.error).toBe(
-          "The request object is missing one or more required attributes",
-        );
-      });
-
-      it("returns 400 when no username and no password are provided", async () => {
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-
-        const res = await supertest(server).post("/auth/login").send({});
-
-        expect(res.status).toBe(400);
-      });
-
-      it("returns error message in res.body when no username and no password are provided", async () => {
-        await supertest(server).post("/auth/register").send({
-          username: "sam",
-          password: "pass",
-          first_name: "Sam",
-          last_name: "Gamgee",
-          email: "baggins@gmail.com",
-          admin: false,
-        });
-
-        const res = await supertest(server).post("/auth/login").send({});
-
-        expect(res.body.error).toBe(
-          "The request object is missing one or more required attributes",
-        );
+  Users.getUserBy("username", username)
+    .then((user) => {
+      if (user) {
+        if (changes.admin) {
+          if (!user.admin) {
+            return res.status(401).json({
+              message:
+                "You are not authorized to change this status. Please contact an admin",
+            });
+          }
+        }
+        Users.editUser(user.user_id, changes)
+          .then((count) => {
+            res
+              .status(200)
+              .json({ message: `Edited ${count} user(s) successfully` });
+          })
+          .catch((err) => {
+            res.status(500).json({
+              error: err.message,
+              errorMessage: "Could not edit that user",
+              stack: "Users router line 44",
+            });
+          });
+      } else {
+        res.status(404).json({ message: "No user with that username exists" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err.message,
+        errorMessage: "Could not edit that user",
+        stack: "Users router line 52",
       });
     });
+});
+
+router.delete("/", (req, res) => {
+  const username = req.jwt.username;
+
+  Users.getUserBy("username", username).then((user) => {
+    Users.deleteUser(user.user_id)
+      .then((delUser) => {
+        res.status(200).json({ message: `${delUser} deleted successfully` });
+      })
+      .catch((err) => {
+        res.status(404).json({ message: "No user with that username exists " });
+      });
   });
 });
+
+module.exports = router;
