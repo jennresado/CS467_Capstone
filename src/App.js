76d748da339ccc4ff178b1c24fde6c061774d752
halekyl <59493297+halekyl@users.noreply.github.com>
2021-07-07 17:55:33 -0400

    Kylee2 (#8)
    * Add Routes for Navigation & Footer NavBar
    
    * Add footer nav bar
    
    * Add top navigation bar
    
    * Add the initial User Profile page, testing navbar
    
    * Bootstrap dependency added and styling for navbar
    
    * remove duplicate
    
    * Added Contact Us page
    
    * Added Sign up page
    
    * Updated User Profile page
    
    * Updated the Top Nav Bar
    
    * Updated the Footer NavBar
    
    * added component imports and routes
    
    * Styling required to space navbar and show content
    
    * Update UserProfile.js
    
    Added new line
    
    * Update Navigation.js
    
    Added New line
    
    * Fixed '#' in url & route Landing/Dshbd as Home
    
    * Contact us page form fields added
    
    * fixing the links to the Landing to /

--- src/App.js
+++ src/App.js
@@ -2,62 +2,51 @@ import { useState, useEffect } from 'react'
 import { useCookies } from 'react-cookie';
 import { BrowserRouter as Router, Route, useHistory } from 'react-router-dom'
 import Landing from './components/Landing'
-import Login from './components/Login'
-import Dashboard from './components/Dashboard'
+import Navigation from './components/Navigation'
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import UserProfile from './components/UserProfile';
+import SignUp from './components/SignUp';
 
 function App() {
-    const [cookies, setCookie, removeCookie] = useCookies(['user'])
-    // removeCookie('user') //when a user logs out
-
-    // Login User
-    const loginUser = async (loginInfo) => {
-        const res = await fetch(
-            `https://bring-me-home-backend.herokuapp.com/auth/login`,
-            {
-                method: 'POST',
-                headers: {'Content-type': 'application/json'},
-                body: JSON.stringify(loginInfo)
-            }
-        )
+  return (
+    <Router>
+      <div className='container'>
+        {/* Navigation Menu compent prop added to each page */}
+        {/* Landing Page */}
         
-        const data = await res.json()
-        
-        if ("token" in data) {
-            // Create cookie
-            setCookie(
-                'user', 
-                {'username': loginInfo.username, 'token': data.token}, 
-                {path: '/', expires: 0}
-            )
-        }
-    }
-
-    return (
-        <Router>
-        <div className='container'>
-            {/* Navigation Menu */}
-            <Route 
-                path='/' exact render={(props) => (
-                    <Landing />
-                )}
-            />
-            {/* Sign Up Page */}
-            <Route 
-                path='/login' 
-                render={(props) => (
-                    <Login 
-                        onLogin={loginUser} 
-                        cookies={cookies}
-                    />
-                )}
-            />
-            {/* Dashboard Page */}
-            <Route path='/dashboard' component={Dashboard} />
-            {/* Profile Settings Page */}
-            {/* Add Animal Page */}
-        </div>
-        </Router>
-    );
+        <Route path= '/' component={Navigation}/>
+        <Route path='/'
+        exact
+        render={(props) => (
+          <>
+          <Landing/>
+          <Route path= '/' component={Footer}/>
+          </>
+        )}
+        />
+        <Route path= '/Landing' component={Landing}/>
+        <Route path= '/Landing' component={Footer}/>
+        <Route path= '/Landing' component={Navigation}/>
+        {/* Sign Up Page */}
+        <Route path= '/SignUp' component={SignUp}/>
+        <Route path= '/SignUp' component={Footer}/>
+        <Route path= '/SignUp' component={Navigation}/>
+        {/* Login Page */}
+        {/* Dashboard Page */}
+        {/* Profile Settings Page */}
+        <Route path= '/UserProfile' component={UserProfile}/>
+        <Route path= '/UserProfile' component={Navigation}/>
+        <Route path= '/UserProfile' component={Footer}/>
+        {/* Add Animal Page */}
+        {/* About Page */}
+        {/* Contact Us Page */}
+        <Route path= '/Contact' component={Contact}/>
+        <Route path= '/Contact' component={Navigation}/>
+        <Route path= '/Contact' component={Footer}/>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
