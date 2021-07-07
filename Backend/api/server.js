8fc22779f7d313a2ecc6e1b10303f49fd0367e52
Jessica <duellje@oregonstate.edu>
2021-07-07 15:18:53 -0400

    set up animals router files

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -6,12 +6,14 @@ const server = express();
 const authRouter = require("../auth/authRouter");
 const auth = require("../auth/authMiddleware");
 const usersRouter = require("../users/usersRouter");
+const animalsRouter = require("../animals/animalsRouter");
 
 server.use(express.json());
 server.use(cors());
 
 server.use("/auth", authRouter);
 server.use("/users", auth, usersRouter);
+server.use("/animals", auth, animalsRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ server: "working" });
