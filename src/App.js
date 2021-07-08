effa7f34c210eb6b1ccd86dc96287b5b2a2c1a29
jennresado <doje@oregonstate.edu>
2021-07-08 16:26:10 -0700

    cleaned up navigation and footer routes in app.js

--- src/App.js
+++ src/App.js
@@ -10,47 +10,83 @@ import SignUp from './components/SignUp';
 import About from './components/About'
 
 function App() {
-  return (
-    <Router>
-      <div className='container'>
-        {/* Navigation Menu compent prop added to each page */}
-        {/* Landing Page */}
-        
-        <Route path= '/' component={Navigation}/>
-        <Route path='/'
-        exact
-        render={(props) => (
-          <>
-          <Landing/>
-          <Route path= '/' component={Footer}/>
-          </>
-        )}
-        />
-        <Route path= '/Landing' component={Landing}/>
-        <Route path= '/Landing' component={Footer}/>
-        <Route path= '/Landing' component={Navigation}/>
-        {/* Sign Up Page */}
-        <Route path= '/SignUp' component={SignUp}/>
-        <Route path= '/SignUp' component={Footer}/>
-        <Route path= '/SignUp' component={Navigation}/>
-        {/* Login Page */}
-        {/* Dashboard Page */}
-        {/* Profile Settings Page */}
-        <Route path= '/UserProfile' component={UserProfile}/>
-        <Route path= '/UserProfile' component={Navigation}/>
-        <Route path= '/UserProfile' component={Footer}/>
-        {/* Add Animal Page */}
-        {/* About Page */}
-        <Route path= '/About' component={About}/>
-        <Route path= '/About' component={Navigation}/>
-        <Route path= '/About' component={Footer}/>
-        {/* Contact Us Page */}
-        <Route path= '/Contact' component={Contact}/>
-        <Route path= '/Contact' component={Navigation}/>
-        <Route path= '/Contact' component={Footer}/>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+        <div className='container'>
+            {/* Navigation Bar */}
+            <Navigation />
+            
+            {/* Landing Page */}
+            <Route 
+                path='/' exact render={(props) => (
+                    <Landing />
+                )}
+            />
+
+            {/* Sign Up Page */}
+            <Route 
+                path= '/SignUp' 
+                render={(props) => (
+                    <SignUp />
+                )
+            }/>
+
+            {/* Login Page */}
+            <Route 
+                path='/login' 
+                render={(props) => (
+                    <Login 
+                        onLogin={loginUser} 
+                        cookies={cookies}
+                    />
+                )}
+            />
+
+            {/* Dashboard Page */}
+            <Route 
+                path= '/Dashboard' 
+                render={(props) => (
+                    <Dashboard />
+                )
+            }/>
+
+            {/* Profile Settings Page */}
+            <Route 
+                path= '/UserProfile' 
+                render={(props) => (
+                    <UserProfile />
+                )
+            }/>
+
+            {/* Add Animal Page */}
+            {/* <Route 
+                path= '/Animal' 
+                render={(props) => (
+                    <Animal />
+                )
+            }/> */}
+            
+            {/* About Page */}
+            <Route 
+                path= '/About' 
+                render={(props) => (
+                    <About />
+                )
+            }/>
+
+            {/* Contact Us Page */}
+            <Route 
+                path= '/Contact' 
+                render={(props) => (
+                    <Contact />
+                )
+            }/>
+
+            {/* Footer */}
+            <Footer />
+        </div>
+        </Router>
+    );
 }
 
 export default App;
