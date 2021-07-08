9308cd399d2045d23c07631b24a6a0c4e7bbd25d
jennresado <doje@oregonstate.edu>
2021-07-07 20:37:45 -0700

    handling login form

--- src/App.js
+++ src/App.js
@@ -4,27 +4,49 @@ import Landing from './components/Landing'
 import Login from './components/Login'
 
 function App() {
-  return (
-    <Router>
-      <div className='container'>
-        {/* Navigation Menu */}
-        <Route 
-            path='/' exact render={(props) => (
-                <Landing />
-            )}
-        />
-        {/* Sign Up Page */}
-        <Route 
-            path='/login' render={(props) => (
-                <Login />
-            )}
-        />
-        {/* Dashboard Page */}
-        {/* Profile Settings Page */}
-        {/* Add Animal Page */}
-      </div>
-    </Router>
-  );
+    // Login User
+    const loginUser = async (loginInfo) => {
+        const res = await fetch(
+            `https://bring-me-home-backend.herokuapp.com/`,
+            {
+                method: 'POST',
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify(loginInfo)
+            }
+        )
+        
+        if (!res.ok) {
+            throw new Error(res.status);
+        }
+        
+        const data = await res.json()
+
+        return data
+    }
+
+    return (
+        <Router>
+        <div className='container'>
+            {/* Navigation Menu */}
+            <Route 
+                path='/' exact render={(props) => (
+                    <Landing />
+                )}
+            />
+            {/* Sign Up Page */}
+            <Route 
+                path='/login' 
+                onLogin={loginUser}
+                render={(props) => (
+                    <Login />
+                )}
+            />
+            {/* Dashboard Page */}
+            {/* Profile Settings Page */}
+            {/* Add Animal Page */}
+        </div>
+        </Router>
+    );
 }
 
 export default App;
