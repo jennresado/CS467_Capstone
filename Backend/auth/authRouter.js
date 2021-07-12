20b3beaad13d93adf7939624df3f66a8d99189ab
Jessica Duell <jessica.duell@pendo.io>
2021-07-12 10:41:27 -0400

    sends user admin status with token when registering

--- Backend/auth/authRouter.js
+++ Backend/auth/authRouter.js
@@ -14,7 +14,7 @@ router.post("/register", validateUser, (req, res) => {
   Users.addUser(user)
     .then(() => {
       const token = signToken(user);
-      res.status(201).json({ message: "Welcome", token });
+      res.status(201).json({ message: "Welcome", token, admin: user.admin });
     })
     .catch((err) => {
       res.status(500).json({
