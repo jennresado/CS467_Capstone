80bf64b66b1359a326f315ee23d6f69a55dff884
Jessica Duell <jessica.duell@pendo.io>
2021-07-15 18:11:48 -0400

    set up dummyRoute for front end

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -8,12 +8,16 @@ const auth = require("../auth/authMiddleware");
 const usersRouter = require("../users/usersRouter");
 const animalsRouter = require("../animals/animalsRouter");
 
+const dummyRouter = require('../dummyRouter');
+
 server.use(express.json({limit: '50mb'}));
 server.use(cors());
 
 server.use("/auth", authRouter);
 server.use("/users", auth, usersRouter);
 server.use("/animals", auth, animalsRouter);
+
+server.use('/dummy/animals', auth, dummyRouter)
 
 server.get("/", (req, res) => {
   res.status(200).json({ server: "working" });
