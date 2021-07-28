f13cb0e1fffbce6d3bbf6d1c85d015afb4ac4dd8
halekyl <59493297+halekyl@users.noreply.github.com>
2021-07-28 16:08:42 -0400

    Update App.js
    Added the path for remove cookie

--- src/App.js
+++ src/App.js
@@ -227,7 +227,7 @@ function App() {
 
         if (res.ok) {   
             const data = await res.json()
-            removeCookie('user')
+            removeCookie('user, {'path': '/'})
             setCookie(
                     'user', 
                     {'username': updateUserInfo.username, 'token': data.new_token, 'admin': data.admin}, 
